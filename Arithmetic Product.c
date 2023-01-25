ef162aae327bc2f96c4d206d840a85fe36c9c716
william-mugo <122535224+william-mugo@users.noreply.github.com>
2023-01-25 22:23:39 +0300

    Add files via upload

---
+++ Arithmetic Product.c
@@ -0,0 +1,13 @@
+// Arithmetic Operation Product
+#include <stdio.h>
+int main() 
+{
+ int a,b,prod;
+ printf("In Enter Value of A");
+ scanf("%d",&a);
+ printf("In Enter Value of B");
+ scanf("%d",&b);
+ prod= a*b;
+ printf("In PROD=%D",prod);
+ return 0;
+}
