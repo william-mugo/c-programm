9e323e26a185246d0a24603edd7ab1a249138fab
william-mugo <122535224+william-mugo@users.noreply.github.com>
2023-02-07 17:35:21 +0300

    Add files via upload

---
+++ divisibility_test_of_7.c
@@ -0,0 +1,14 @@
+// Divisibility Test of 7
+#include <stdio.h>
+int main () {
+    int a;
+    printf("|n Enter Value of a:");
+    scanf("%d",&a);
+    if("a%7==o"){
+    printf("\n a is divisible by 7:");
+    }
+   else{
+   printf("\n a is not divisible by 7:");
+   }
+    return 0;
+}
