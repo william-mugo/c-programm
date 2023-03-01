6cb53048d4e7cb699005a9b1b050bc5fcaed3dcb
william-mugo <122535224+william-mugo@users.noreply.github.com>
2023-03-01 08:52:52 -0800

    Add files via upload

---
+++ Product_function.c
@@ -0,0 +1,14 @@
+// Function of Product of Two Numbers
+# include <stdio.h>
+int main ()
+{
+    int answer;
+    answer=product();
+    printf("\n product of two numbers is answer");
+    return 0;
+}
+    int product ()
+{
+    int x = 10,y = 2;
+    return x * y;
+}
