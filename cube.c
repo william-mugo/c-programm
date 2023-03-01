fc38c89a388d3e3a6d99b36dd049d6bf29a69467
william-mugo <122535224+william-mugo@users.noreply.github.com>
2023-03-01 07:55:59 -0800

    Add files via upload

---
+++ cube.c
@@ -0,0 +1,14 @@
+// Cube of Given Integer
+# include <stdio.h>
+int main ()
+{
+    int num=0;
+    do
+    {
+        printf("\n %d ",num);
+        num=num+1;
+        printf("Number is:%d and cube of the %d is:%d,\n",num,num,(num*num*num));
+    }
+        while(num<=4);
+    return 0;
+}
